c74c5a69caf1780e87324097312c326b8e40dab9
singh1shardul <shardul.conclusion1@gmail.com>
2026-04-05 19:37:03 +0530

    Remove figma_components from tracking and remote

--- src/figma_components/EqualizerIcon.tsx
+++
@@ -1,9 +0,0 @@
-export default function EqualizerIcon({ className = "" }: { className?: string }) {
-  return (
-    <div className={`flex items-end justify-center gap-0.5 ${className}`}>
-      <div className="w-1 bg-current rounded-full animate-equalizer-1" style={{ height: '60%' }} />
-      <div className="w-1 bg-current rounded-full animate-equalizer-2" style={{ height: '100%' }} />
-      <div className="w-1 bg-current rounded-full animate-equalizer-3" style={{ height: '80%' }} />
-    </div>
-  );
-}
